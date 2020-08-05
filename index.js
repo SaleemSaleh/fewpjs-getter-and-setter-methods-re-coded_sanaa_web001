ef1bb8e0ffd408e6d4b3cb71f7c83691d36b501f
Saleem Saleh <salem1234saleem@gmail.com>
2020-08-05 15:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ class Circle {
   constructor(radius){
     this.radius = radius;
   }
-
+  get diameter() {
+      return 2 * this.radius;
+    }
+  get circumference() {
+        return 2 * Math.PI * this.radius;
+      }
+  get area () {
+        return  Math.PI * this.radius * this.radius;
+    }
 }
